8702404e3aa6ff2db87d1109a718ac9fe139476a
fboyle2001 <finlayboyle2001@gmail.com>
2022-01-26 08:37:23 +0000

    Remove display

--- code/main.py
+++ code/main.py
@@ -41,7 +41,7 @@ def run():
     }
 
     env = gym.make(args.env_id)
-    env = gym.wrappers.Monitor(env, f"./videos/", video_callable=lambda ep_id: ep_id%10 == 0, force=True)
+    # env = gym.wrappers.Monitor(env, f"./videos/", video_callable=lambda ep_id: ep_id%10 == 0, force=True)
 
     log_dir = os.path.join(
         'logs', args.env_id,
